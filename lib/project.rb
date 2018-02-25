89d52e2a5073351035b929a4eb8b80e9ebab0735
David Lewis <davidlewisp@gmail.com>
2018-02-25 15:33:46 -0800

    work on getting third spec to pass, returning an integer but not passing the spec

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
-  
-attr_accessor :title
-attr_reader :id
+
+attr_accessor :title, :id
 
   def initialize(attributes)
     @title = attributes[:title]
     @id = attributes[:id]
+  end
+
+  def save
+    var = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = var.first.fetch('id').to_i
   end
 end
