c83f86dde23ca0faaafc766e5bf7b0476439e165
David Lewis <davidlewisp@gmail.com>
2018-03-03 10:01:54 -0800

    pass 11th spec to delete project

--- lib/project.rb
+++ lib/project.rb
@@ -55,4 +55,8 @@ attr_accessor :title, :id
     @title = project[:title]
     DB.exec("UPDATE projects SET title = ('#{@title}') WHERE id = ('#{@id}');")
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = ('#{@id}');")
+  end
 end
