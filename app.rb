646d0911d2de890ba5078542e2e8d3d8d43b3401
David Lewis <davidlewisp@gmail.com>
2018-03-04 12:29:01 -0800

    Pass integration spec to update a project

--- app.rb
+++ app.rb
@@ -19,3 +19,20 @@ post('/') do
   @projects = Project.all
   erb(:index)
 end
+
+get('/project/:id') do
+  @project = Project.find(params['id'].to_i)
+  erb(:projects)
+end
+
+get('/project/:id/edit') do
+  @project = Project.find(params['id'].to_i)
+  erb(:edit_project)
+end
+
+patch('/project/:id') do
+  @project = Project.find(params['id'].to_i)
+  new_title = params['title']
+  @project.update({:title => new_title, :id => @project.id})
+  erb(:projects )
+end
